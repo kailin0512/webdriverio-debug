31dd8bcee9345bf653b832d7e96d7758374ddf69
kailin <kailin@ebay.com>
2021-05-05 15:21:57 -0400

    Minimal reproducible

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,15 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        browser.reloadSession();
+        browser.url(`https://the-internet.herokuapp.com/login`);
+
+        $('#username').setValue('tomsmith');
+        $('#password').setValue('SuperSecretPassword!');
+        $('button[type="submit"]').click();
+
+        expect($('#flash')).toBeExisting();
+        expect($('#flash')).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
